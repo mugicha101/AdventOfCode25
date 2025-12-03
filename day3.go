f9d539393d8cefae9e421869d0b3ac38f739244f
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-12-03 00:34:27 -0500

    optimize

--- day3.go
+++ day3.go
@@ -1,13 +1,17 @@
 package main
 
-func Day3A(io *IO) {
+func solve(io *IO, digs int) {
 	var line string
-	res := 0
+	var res int64 = 0
 	for io.Read(&line) != nil {
-		mv := 0
-		for i := 0; i < len(line); i++ {
-			for j := i + 1; j < len(line); j++ {
-				mv = max(mv, int(line[i]-'0')*10+int(line[j]-'0'))
+		// dp: (index, num vals left) -> max value
+		var dp = make([]int64, digs+1)
+		mv := int64(0)
+		for _, c := range line {
+			v := int64(c - '0')
+			for i := 0; i < digs; i++ {
+				dp[i] = max(dp[i], dp[i+1]*10+v)
+				mv = max(mv, dp[i])
 			}
 		}
 		res += mv
@@ -15,23 +19,10 @@ func Day3A(io *IO) {
 	io.Write("%d\n", res)
 }
 
+func Day3A(io *IO) {
+	solve(io, 2)
+}
+
 func Day3B(io *IO) {
-	var line string
-	var res int64 = 0
-	for io.Read(&line) != nil {
-		// dp: (index, num vals left) -> max value
-		var dp [13]int64
-		for _, c := range line {
-			v := int64(c - '0')
-			for i := 0; i < 12; i++ {
-				dp[i] = max(dp[i], dp[i+1]*10+v)
-			}
-		}
-		mv := int64(0)
-		for i := 0; i < 12; i++ {
-			mv = max(mv, dp[i])
-		}
-		res += mv
-	}
-	io.Write("%d\n", res)
+	solve(io, 12)
 }
