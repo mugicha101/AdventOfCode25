7df13a2f39afa639709ec9b51e1af1e0e590f720
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-12-12 00:39:52 -0500

    optimize day 12

--- day12.go
+++ day12.go
@@ -2,70 +2,45 @@ package main
 
 import "strings"
 
-type shape struct {
-	grid [3][3]bool
-	size int
-}
-
-type region struct {
-	rows, cols int
-	shapeCount [6]int
-}
-
-func day12input(io *IO) ([6]shape, []region) {
-	var shapes [6]shape
+func Day12(io *IO) {
+	// size of grid >= num of total needed tiles
+	// if num of shapes <= number of 3x3 regions in the area, is trivial to fit
+	var tilesPerShape [6]int
 	var line string
-	for i := 0; i < len(shapes); i++ {
+	for i := 0; i < len(tilesPerShape); i++ {
 		io.Readln(&line)
 		for r := 0; r < 3; r++ {
 			io.Readln(&line)
 			for c := 0; c < 3; c++ {
 				if line[c] == '#' {
-					shapes[i].grid[r][c] = true
-					shapes[i].size++
+					tilesPerShape[i]++
 				}
 			}
 		}
 		io.Readln(&line)
 	}
-	regions := make([]region, 0)
+	res := 0
 	for io.Readln(&line) == nil {
-		io.Write("%s\n", line)
 		x := strings.Index(line, "x")
 		c := strings.Index(line, ":")
-		reg := region{}
-		reg.rows = stoi(line[:x])
-		reg.cols = stoi(line[x+1 : c])
-		for i, val := range strings.Split(line[c+2:], " ") {
-			reg.shapeCount[i] = stoi(val)
-		}
-		regions = append(regions, reg)
-	}
-	return shapes, regions
-}
-
-func Day12(io *IO) {
-	// size of grid >= num of total needed tiles
-	// if num of shapes <= number of 3x3 regions in the area, is trivial to fit
-	shapes, regions := day12input(io)
-	res := 0
-	for _, reg := range regions {
+		rows := stoi(line[:x])
+		cols := stoi(line[x+1 : c])
 		numShapes := 0
 		numTiles := 0
-		for i, cnt := range reg.shapeCount {
+		for i, val := range strings.Split(line[c+2:], " ") {
+			cnt := stoi(val)
 			numShapes += cnt
-			numTiles += cnt * shapes[i].size
+			numTiles += cnt * tilesPerShape[i]
 		}
-		io.Write("numShapes: %d, numTiles: %d, area: %d\n", numShapes, numTiles, reg.rows*reg.cols)
-		area := reg.rows * reg.cols
-		if numShapes <= (reg.rows/3)*(reg.cols/3) {
+
+		if numShapes <= (rows/3)*(cols/3) {
 			// possible
 			res++
-		} else if numTiles >= area {
+		} else if numTiles >= rows*cols {
 			// impossible
 		} else {
 			// np hard
-			panic("Nope!")
+			panic("Nope!") // doesn't work for samples lol
 		}
 	}
 	io.Write("%d\n", res)
